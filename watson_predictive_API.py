cfa673d83d18bee3a27cb29614ccd600032127ed
Tom Sha-bar <tomshalbar@gmail.com>
2024-10-26 02:31:52 -0400

    Made a change to the WastonAPi

--- watson_predictive_API.py
+++ watson_predictive_API.py
@@ -1,6 +1,31 @@
 import json 
+import requests
+from ibm_watsonx_ai import APIClient
+from ibm_watsonx_ai import Credentials
+from ibm_watsonx_ai.foundation_models import ModelInference
 
 with open("fake-json-data.json", "r") as json_file:
     data = json.load(json_file)
 
-print(data["response"][0]["pageName"])
+
+credentials = Credentials(
+    url = "https://us-south.ml.cloud.ibm.com",
+    api_key = "kk-R4zCw_li1eSehAixwX3M15OkVpR0dMLt2AAloX57q",
+)
+
+client = APIClient(credentials)
+
+model = ModelInference(
+  model_id="ibm/granite-13b-chat-v2",
+  api_client=client,
+  project_id="123a2a50-82f2-4fc8-bd6e-aebd36c91c1b",
+  params = {
+      "max_new_tokens": 100
+  }
+)
+
+disaster = data["response"][1]["title"]
+print(disaster)
+prompt = f"How likely is {disaster} to be a natural disaster case?"
+print(model.generate(prompt))
+print(model.generate_text(prompt))
