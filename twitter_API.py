06ca833532ff9b9e0c26717bc872309ad03b7855
Yart <134120826+EAHowes@users.noreply.github.com>
2024-10-27 13:30:33 -0400

    Update twitter_API.py
    return the following error: 
    
    Error posting tweet: 403 Forbidden
    453 - You currently have access to a subset of Twitter API v2 endpoints and limited v1.1 endpoints (e.g. media post, oauth) only. If you need access to this endpoint, you may need a different access level. You can learn more here: https://developer.twitter.com/en/portal/product

--- twitter_API.py
+++ twitter_API.py
@@ -1,9 +1,22 @@
 import tweepy
 
-CONSUMER_KEY = "Y3tVqGSLUdEvCuYuJ5m7NIoAt" #Your API/Consumer key 
-CONSUMER_SECRET = "HcMo1Utle3m95IzKJNXn9qSpuNaGCCh17f5lUFSKCviwii9VY3" #Your API/Consumer Secret Key
-ACCESS_TOKEN = "1810424105482784768-0vRw1I8Er2dKuJogCNFz3mybKyJA3o" #Your Access token key
-ACCESS_TOKEN_SECRET = "IUID4xcIq9aZG06XNr6lggsmhffO1TRcvk7ki1Vm3dRNI" #Your Access token Secret key
+api_key = "Y3tVqGSLUdEvCuYuJ5m7NIoAt" #Your API/Consumer key 
+api_key_secret = "HcMo1Utle3m95IzKJNXn9qSpuNaGCCh17f5lUFSKCviwii9VY3" #Your API/Consumer Secret Key
+access_token = "1810424105482784768-0vRw1I8Er2dKuJogCNFz3mybKyJA3o" #Your Access token key
+access_token_secret = "IUID4xcIq9aZG06XNr6lggsmhffO1TRcvk7ki1Vm3dRNI" #Your Access token Secret key
 
 # filtered data will be used to generate posts
 
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+def post_tweet(content):
+    try:
+        api.update_status(content)
+        print("posted tweet")
+    except tweepy.errors.TweepyException as error:
+        print(f"Error posting tweet: {error}")
+
+post_tweet("Hello this is a test post")
