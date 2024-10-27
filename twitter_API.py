b7b07d0360dd7b60c3a461842bcf4606e178bf0a
OvidioCalvet <calvetovidio@gmail.com>
2024-10-26 22:29:54 -0400

    initialized twitter api info

--- twitter_API.py
+++ twitter_API.py
@@ -5,5 +5,5 @@ CONSUMER_SECRET = "HcMo1Utle3m95IzKJNXn9qSpuNaGCCh17f5lUFSKCviwii9VY3" #Your API
 ACCESS_TOKEN = "1810424105482784768-0vRw1I8Er2dKuJogCNFz3mybKyJA3o" #Your Access token key
 ACCESS_TOKEN_SECRET = "IUID4xcIq9aZG06XNr6lggsmhffO1TRcvk7ki1Vm3dRNI" #Your Access token Secret key
 
-
+# filtered data will be used to generate posts
 
