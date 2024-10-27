6bf8536e090b15e2120e56e72ab8218063a8586d
tomshalbar <tomshalbar@gmail.com>
2024-10-27 15:49:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import web_scraper
 import time
 
 while True:
-    watsonInteraction.create_disaster_response()
     web_scraper.lookThroughWeb()
+    watsonInteraction.create_disaster_response()
     time.sleep(300)
 
 
