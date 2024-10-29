9f764a218525f09f1c4a3d8ad51603971a6b0bb5
tomshalbar <tomshalbar@gmail.com>
2024-10-29 09:56:18 -0400

    Update twitter_API.py

--- twitter_API.py
+++ twitter_API.py
@@ -1,9 +1,9 @@
 import tweepy
 
-api_key = "Y3tVqGSLUdEvCuYuJ5m7NIoAt" #Your API/Consumer key 
-api_key_secret = "HcMo1Utle3m95IzKJNXn9qSpuNaGCCh17f5lUFSKCviwii9VY3" #Your API/Consumer Secret Key
-access_token = "1810424105482784768-0vRw1I8Er2dKuJogCNFz3mybKyJA3o" #Your Access token key
-access_token_secret = "IUID4xcIq9aZG06XNr6lggsmhffO1TRcvk7ki1Vm3dRNI" #Your Access token Secret key
+api_key = #Your API/Consumer key 
+api_key_secret = #Your API/Consumer Secret Key
+access_token = #Your Access token key
+access_token_secret = #Your Access token Secret key
 
 # filtered data will be used to generate posts
 
